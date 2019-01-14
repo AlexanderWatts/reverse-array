7483a827de3dd15da22486c550efb8f5ef769e0d
Alexander Watts <alexcharleswatts@gmail.com>
2019-01-14 00:05:25 +0000

    Completed algorithm to reverse an array of integers

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main {
     public static void main(String[] args) {
         
         ReverseArray reverse = new ReverseArray();
+        reverse.reverseArray(new int[]{1, 2, 3, 4, 5});
 
     }
 
--- ReverseArray.java
+++ ReverseArray.java
@@ -1,3 +1,55 @@
+import java.util.Arrays;
+
 class ReverseArray {
+
+    private int[] numbers;
     
+    /**
+     * Reverse an array of integers
+     * So [1, 2, 3] becomes [3, 2, 1]
+     * @param arr
+     */
+    public void reverseArray(int[] arr) {
+        numbers = arr;
+        reverse();
+        //Print sorted array
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public void reverse() {
+
+        //Set pointers
+        int left = 0;
+        int right = numbers.length - 1;
+
+        //As long as left is less than right
+        //keep swapping the integers at each index
+        while(left < right) {
+            //Swap values
+            swap(left, right);
+            //Move left pointer up
+            left++;
+            //Move right pointer down
+            right--;
+        }
+
+    }
+
+    /**
+     * Swap two values in array given the indexs
+     * @param left The left pointers index
+     * @param right The right pointers index
+     */
+    public void swap(int left, int right) {
+
+        //Temporarily store the left pointers value
+        int temp = numbers[left];
+
+        //Overwrite the left pointers value
+        numbers[left] = numbers[right];
+
+        //Set the last pointers value to the left pointers
+        numbers[right] = temp;
+    }
+
 }
